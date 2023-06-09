4818313e9fc6e67aff9df1589071ce713c158a7e
Solomon Israel <solomonisrael0012@gmail.com>
2023-06-09 04:25:39 +0100

    OAuth google functionality signup

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,7 +1,38 @@
-import {FcGoogle} from "react-icons/fc"
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify"
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 export default function OAuth() {
+  const navigate = useNavigate()
+  async function onnGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user
+      
+      // check for the user
+
+      const docRef = doc(db, "users", user.uid)
+      const docSnap = await getDoc(docRef)
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+    navigate("/")
+  }
   return (
-    <button className="flex 
+    <button type="button" onClick={onnGoogleClick} className="flex 
       items-center justify-center 
       w-full bg-red-700 text-white 
       px-7 py-3 uppercase text-sm 
