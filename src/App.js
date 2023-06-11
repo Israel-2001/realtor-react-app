986d7c8ec2daf9819ba9995610de5ed4ad172cda
Solomon Israel <solomonisrael0012@gmail.com>
2023-06-11 03:28:45 +0100

    creating private route for protecting the profile page and logout functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/sign-in";
 import SignUp from "./pages/sign-up";
+import PrivateRoute from "./components/PrivateRoute";
 import ForgotPassword from "./pages/forgot-password";
 import Offers from "./pages/offers";
 import Header from "./components/Header";
@@ -12,14 +13,17 @@ function App() {
   return (
     <>
       <Router>
-        <Header path="/header" element={<Header /> } />
+        <Header />
         <Routes>
           <Route path="/" element={<Home /> } />
-          <Route path="/offers" element={<Offers /> } />
+          <Route path="/profile" element={<PrivateRoute />} >
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/profile" element={<Profile /> } />
           <Route path="/sign-in" element={<SignIn /> } />
           <Route path="/sign-up" element={<SignUp /> } />
           <Route path="/forgot-password" element={<ForgotPassword /> } />
-          <Route path="/profile" element={<Profile /> } />
+          <Route path="/offers" element={<Offers /> } />
         </Routes>
       </Router>
       <ToastContainer
