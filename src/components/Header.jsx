688286b384223868965c704098d852464e24495b
Solomon Israel <solomonisrael0012@gmail.com>
2023-07-31 20:07:47 +0100

    changing of logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,15 +24,22 @@ export default function Header() {
     <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
-          <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" className="h-5 cursor-pointer" onClick={()=>navigate("/")} />
+          <p className="h-5 cursor-pointer font-bold" onClick={()=>navigate("/")}>HOUSING</p>
         </div>
+        {/* <nav>
+          <ul>
+            <li>Home</li>
+            <li>Offers</li>
+            <li>Profile</li>
+          </ul>
+        </nav> */}
         <div>
           <ul className="flex space-x-10">
             <li 
               className={`cursor-pointer py-3 
               text-sm font-semibold text-gray-400 border-b-[3px]
                border-b-transparent ${pathMatchRoutes("/") && 
-               "text-stone-950 border-red-500 font-bold"}`} 
+               "text-stone-950 border-red-600 font-bold"} max-sm:text-[10px]`} 
                onClick={()=>navigate("/")}>
                 Home
             </li>
@@ -40,7 +47,7 @@ export default function Header() {
               className={`cursor-pointer py-3 
               text-sm font-semibold text-gray-400 border-b-[3px] 
               border-b-transparent ${pathMatchRoutes("/offers") && 
-              "text-stone-950 border-b-red-500 font-bold" }`} 
+              "text-stone-950 border-b-red-600 font-bold" } max-sm:text-[10px]`} 
               onClick={()=>navigate("/offers")}>
                 Offers
             </li>
@@ -48,7 +55,7 @@ export default function Header() {
               className={`cursor-pointer py-3 
               text-sm font-semibold text-gray-400 border-b-[3px] 
               border-b-transparent ${(pathMatchRoutes("/sign-in") || pathMatchRoutes("/profile")) && 
-              "text-stone-950 border-b-red-500 font-bold" }`} 
+              "text-stone-950 border-b-red-600 font-bold" } max-sm:text-[10px]`} 
               onClick={()=>navigate("/profile")}>
                 {pageState}
             </li>
